6e5c559b67abf98b0e0672d80850224021ab750f
ahmedFATHY <af773942@gmail.com>
2024-08-12 20:40:55 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import NotFound from './pages/NotFound'
 
 import './App.css'
 import { Routes } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 
 function App() {
 
